60c87e0d4b795a37e9c475344fc5419804400c8b
Nick Dumas <nobodys@business.com>
2024-10-15 22:17:56 -0400

    add tests for keeping and dropping

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -45,3 +45,71 @@ func Test_Simple(t *testing.T) {
 		})
 	}
 }
+
+func Test_Dropping(t *testing.T) {
+	logger := slog.New(slog.NewTextHandler(os.Stderr, nil))
+	cases := []struct {
+		input    string
+		expected []Token
+	}{
+		{
+			input: "dh1d20", expected: []Token{
+				{dropHighestToken, "dh"},
+				{numberToken, "1"},
+				{dieToken, "d"},
+				{numberToken, "20"},
+			},
+		},
+		{
+			input: "dl2d4", expected: []Token{
+				{dropLowestToken, "dl"},
+				{numberToken, "2"},
+				{dieToken, "d"},
+				{numberToken, "4"},
+			},
+		},
+	}
+
+	for _, tc := range cases {
+		t.Run(tc.input, func(t *testing.T) {
+			actual := Lex(tc.input, logger).Items()
+			if !reflect.DeepEqual(actual, tc.expected) {
+				t.Fail()
+			}
+		})
+	}
+}
+
+func Test_Keeping(t *testing.T) {
+	logger := slog.New(slog.NewTextHandler(os.Stderr, nil))
+	cases := []struct {
+		input    string
+		expected []Token
+	}{
+		{
+			input: "kh1d20", expected: []Token{
+				{keepHighestToken, "kh"},
+				{numberToken, "1"},
+				{dieToken, "d"},
+				{numberToken, "20"},
+			},
+		},
+		{
+			input: "kl2d4", expected: []Token{
+				{keepLowestToken, "kl"},
+				{numberToken, "2"},
+				{dieToken, "d"},
+				{numberToken, "4"},
+			},
+		},
+	}
+
+	for _, tc := range cases {
+		t.Run(tc.input, func(t *testing.T) {
+			actual := Lex(tc.input, logger).Items()
+			if !reflect.DeepEqual(actual, tc.expected) {
+				t.Fail()
+			}
+		})
+	}
+}
